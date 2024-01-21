dd477708fd56706ac30db318ec464b4742f8e8f9
root <rabbitsinwarrens@gmail.com>
2024-01-20 19:11:40 -0800

    test(capture): fail create capture object
    Need to fix compilation errors:
    - Undefined 'CaptureObject'
    - undeclared 'create_capture_object'
    - incorrect pointer assertion.

--- tests/test_capture_stdout.c
+++ tests/test_capture_stdout.c
@@ -1,11 +1,18 @@
 #include "unity.h"
 
+
 void setUp(void)
 {
 }
 
 void tearDown(void)
 {
+}
+
+void test_that_CaptureObject_IsCreated()
+{
+	CaptureObject sut = create_capture_object(stdout);
+	TEST_ASSERT_NOT_NULL(sut);
 }
 
 int main(void)
